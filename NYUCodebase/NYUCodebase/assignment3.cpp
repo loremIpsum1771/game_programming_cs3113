314d1083ebe5181cd169420badb553d7e1977ef4
Elijah Hawthorne <ceh451@nyu.edu>
2016-03-16 18:05:47 -0400

    general functionality implemented, still issues with collisions

--- NYUCodebase/NYUCodebase/assignment3.cpp
+++ NYUCodebase/NYUCodebase/assignment3.cpp
@@ -36,14 +36,14 @@ public:
 		:textureID(textureID), u(u), v(v), width(width), height(height), size(size), program(program) {
 
 		/*SheetSprite(unsigned int textureID, float u, float v, float width, float height, float size)
-			:textureID(textureID), u(u), v(v), width(width), height(height), size(size)*/
+		:textureID(textureID), u(u), v(v), width(width), height(height), size(size)*/
 
 
-		u = 0;
+		/*	u = 0;
 		v = 0;
 		width = 0;
 		height = 0;
-		size = 0;
+		size = 0;*/
 	}
 
 	void Draw() {
@@ -107,8 +107,6 @@ public:
 	SheetSprite mySprite;
 	ShaderProgram* program;
 
-	
-
 
 
 	virtual void Update(float elapsed) {
@@ -118,18 +116,89 @@ public:
 
 
 	}
+
 	void Render() {
 		// for all game elements
 		// setup transforms, render sprites
 	}
 
 	void drawSprite() {
-		modelM.Scale(3.0, 3.0, 0.0);
+		program->setModelMatrix(modelM);
+		//program->setProjectionMatrix(projectionMatrix);
+		//program->setViewMatrix(viewMatrix);
+		modelM.identity();
+		modelM.Translate(x, y, 0.0);
+		modelM.Scale(width, height, 0.0);
 		mySprite.Draw();
 
 	}
+};
+
+
+class Spaceship : public Entity {
+public:
+
+
+	Spaceship(float xDirect, float yDirect, float rState, float width, float height, float speed,
+		float xPosition, float yPosition, Matrix &modelMatrix, ShaderProgram* program, SheetSprite newSprite, bool movingLeft, bool movingRight) :
+		Entity(xDirect, yDirect, rState, width, height, speed, xPosition, yPosition, modelMatrix, program, newSprite), movingLeft(movingLeft), movingRight(movingRight) {}
+
+	virtual void update(float elapsed) {
+
+	}
+
+	bool movingLeft;
+	bool movingRight;
+};
+
+class Enemy : public Entity {
+public:
+	Enemy(float xDirect, float yDirect, float rState, float width, float height, float speed,
+		float xPosition, float yPosition, Matrix &modelMatrix, ShaderProgram* program, SheetSprite newSprite);
+	bool isSolid;
+	bool floatingLeft;
+	bool floatingRight;
+	int leftmost;
+	int rightmost;
+	int vectPosX;
+	int vectPosY;
 
 };
+
+
+
+
+
+
+
+class Bullet : public Entity {
+
+public:
+
+	Bullet(float xDirect, float yDirect, float rState, float width, float height, float speed,
+		float xPosition, float yPosition, Matrix &modelMatrix, ShaderProgram* program, SheetSprite newSprite, float angle, float timeAlive)
+		: Entity(xDirect, yDirect, rState, width, height, speed, xPosition, yPosition, modelMatrix, program, newSprite), angle(angle), timeAlive(timeAlive) {
+		//angle = 0.0;
+		//timeAlive = 0.0;
+	}
+
+	float angle;
+	float timeAlive;
+
+};
+
+Enemy::Enemy(float xDirect, float yDirect, float rState, float width, float height, float speed,
+	float xPosition, float yPosition, Matrix &modelMatrix, ShaderProgram* program, SheetSprite newSprite) :
+	Entity(xDirect, yDirect, rState, width, height, speed, xPosition, yPosition, modelMatrix, program, newSprite) {
+	leftmost = 0;
+	rightmost = 10;
+	vectPosX = 0;
+	vectPosY = 0;
+	isSolid = true;
+	floatingLeft = false;
+	floatingRight = true;
+}
+std::vector<Enemy*> entities;
 
 
 void drawText(ShaderProgram *program, int fontTexture, std::string text, float size, float spacing) {
@@ -167,12 +236,181 @@ void drawText(ShaderProgram *program, int fontTexture, std::string text, float s
 	glDisableVertexAttribArray(program->texCoordAttribute);
 }
 
+std::vector<Bullet*> bullets;
 
-// x     y
-std::vector<float> translations = { 1.0, -1.0,
--1.0, -1.0,
--1.0, 1.0, };
+void shootBullet(ShaderProgram* program, SheetSprite b_sprite, Spaceship* aShip, Matrix &modelM) {
+	//Bullet(float xDirect, float yDirect, float rState, float width, float height, float speed, float xPosition, float yPosition, Matrix &modelMatrix, ShaderProgram* program, SheetSprite newSprite, float angle, float timeAlive)
+	Bullet* newBullet = new Bullet(1.0, 1.0, 0.0, 0.5, 0.5, 4.0, aShip->x, aShip->y, modelM, program, b_sprite, 0.0, 0.0);
 
+	bullets.push_back(newBullet);
+}
+
+bool shouldRemoveBullet(Bullet* bullet) {
+	float maxY = 2.0;
+
+	if ((bullet->y) >= maxY) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+bool checkCollision(Enemy* anEnemy, Bullet* aBullet) {
+	float enemyTop = anEnemy->y + (anEnemy->height / 2.0f);
+	float enemyBottom = anEnemy->y - (anEnemy->height / 2.0f);
+	float enemyRight = anEnemy->x + (anEnemy->width / 2.0f);
+	float enemyLeft = anEnemy->x - (anEnemy->width / 2.0f);
+
+	float bulletTop = aBullet->y + (aBullet->height / 2.0f);
+	float bulletBottom = aBullet->y - (aBullet->height / 2.0f);
+	float bulletLeft = aBullet->x - (aBullet->width / 2.0f);
+	float bulletRight = aBullet->x + (aBullet->width / 2.0f);
+	
+	//This is supposed to check that if the objects are not intersecting, return false
+	if (
+		(bulletBottom > enemyTop) ||
+		(bulletTop < enemyBottom) ||
+		(bulletLeft > enemyRight) ||
+		(bulletRight < enemyLeft)
+		) {
+		return false;
+	}
+	else {
+		return true;
+	}
+}
+
+
+
+void UpdateGameLevel(ShaderProgram* program,  float &elapsed,  Spaceship* spaceship, std::vector<Enemy*>& entities, Matrix &modelM, std::string &text) {
+	
+	float minX = -3.50f;
+	float maxX = 3.50f;
+
+	//move the enemies
+	for (int i = 0; i < entities.size(); i++) {
+		if (entities[i] != nullptr) {
+			entities[i]->x += elapsed * 0.3 * entities[i]->xDirection;
+			if (entities[i]->x >= maxX || entities[i]->x <= minX) {
+				for (int k = 0; k < entities.size(); k++) {
+					if (entities[k] != nullptr) {
+						entities[k]->xDirection *= -1;
+					}
+				}
+				for (int j = 0; j < entities.size(); j++) {
+					if (entities[j] != nullptr) {
+						entities[j]->y -= 0.02;
+					}
+				}
+			}
+		}
+	}
+
+	//move the bullets
+	float bulletTop, bulletBottom, bulletLeft, bulletRight;
+	for (int i = 0; i < bullets.size(); i++) {
+
+		if (bullets[i] != nullptr) {
+			if (shouldRemoveBullet(bullets[i])) {
+				delete bullets[i];
+				bullets[i] = nullptr;
+			}
+		}
+		if (bullets[i] != nullptr) {
+			if (bullets[i]->y < 0) {
+				bullets[i]->y += elapsed * bullets[i]->speed * 1;
+
+			}
+			else if (bullets[i]->y >= 0) {
+				bullets[i]->y += elapsed * bullets[i]->speed;
+				//std::cout << "bullets position: " << bullets[i]->y << std::endl;
+			}
+		}
+		}
+	//Check if each of the bullets has hit any of the enemies
+	for (int i = 0; i < bullets.size(); i++) {
+		for (int j = 0; j < entities.size(); j++) {
+			if (entities[j] != nullptr && bullets[i] != nullptr) {
+				if (checkCollision(entities[j], bullets[i])) {
+					std::cout << "bullets y position: " << bullets[i]->y << std::endl;
+					std::cout << " hit enemy y position: " << entities[j]->y << std::endl;
+					std::cout << "bullets x position: " << bullets[i]->x << std::endl;
+					std::cout << " hit enemy x position: " << entities[j]->x << std::endl;
+					delete entities[j];
+					entities[j] = nullptr;
+					delete bullets[i];
+					bullets[i] = nullptr;
+				}
+			}
+		}
+	}
+
+
+}
+
+void RenderGameLevel(ShaderProgram* program, Matrix &modelM, std::string &text,  Spaceship* spaceship, std::vector<Enemy*> entities) {
+	spaceship->drawSprite();
+
+
+
+	for (int i = 0; i < entities.size(); i++) {
+		if (entities[i] != nullptr) {
+			entities[i]->drawSprite();
+		}
+	}
+
+	for (int i = 0; i < bullets.size(); i++) {
+		if (bullets[i] != nullptr) {
+			bullets[i]->drawSprite();
+		}
+	}
+}
+
+enum GameState { STATE_MAIN_MENU, STATE_GAME_LEVEL };
+int state;
+
+
+void Update(ShaderProgram* program, float &elapsed,  Spaceship* spaceship, std::vector<Enemy*>& entities, Matrix &modelM, std::string &text) {
+	const Uint8 *keys = SDL_GetKeyboardState(NULL);
+	switch (state) {
+	case STATE_MAIN_MENU:
+
+		//std::cout << "Game Started" << std::endl;
+		if (keys[SDL_SCANCODE_S]) {
+			state = STATE_GAME_LEVEL;
+		}
+
+		//UpdateMainMenu();
+		break;
+
+	case STATE_GAME_LEVEL:
+		//UpdateGameLevel(ShaderProgram* program, float &elapsed, Spaceship* spaceship, std::vector<Enemy*>& entities, Matrix &modelM, std::string &text) {
+		UpdateGameLevel(program, elapsed,  spaceship, entities, modelM, text);
+		break;
+	}
+
+
+}
+
+void Render(ShaderProgram* program, Matrix &modelM,  std::string &text,  Spaceship* spaceship, std::vector<Enemy*> entities, int fontTexture, float size, float spacing) {
+	text = "Space Invaders\nPress space to play";
+	glClear(GL_COLOR_BUFFER_BIT);
+	switch (state) {
+	case STATE_MAIN_MENU:
+		//RenderMainMenu();
+		std::cout << "Main Menu" << std::endl;
+		drawText(program, fontTexture, text, size, spacing);
+		break;
+
+	case STATE_GAME_LEVEL:
+		
+		RenderGameLevel(program, modelM, text, spaceship, entities);
+
+		break;
+	}
+
+}
 
 
 
@@ -182,123 +420,144 @@ int main(int argc, char *argv[])
 	float lastFrameTicks = 0.0f;
 
 	SDL_Init(SDL_INIT_VIDEO);
-	displayWindow = SDL_CreateWindow("My Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 360, SDL_WINDOW_OPENGL);
+	displayWindow = SDL_CreateWindow("My Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 690, SDL_WINDOW_OPENGL);
 	SDL_GLContext context = SDL_GL_CreateContext(displayWindow);
 	SDL_GL_MakeCurrent(displayWindow, context);
 #ifdef _WINDOWS
 	glewInit();
 #endif
-	glViewport(0, 0, 640, 360);
+	glViewport(0, 0, 1280, 690);
 	ShaderProgram* program = new ShaderProgram(RESOURCE_FOLDER"vertex_textured.glsl", RESOURCE_FOLDER"fragment_textured.glsl");
 	
-
 	Matrix projectionMatrix;
 	Matrix modelMatrix;
-	//modelMatrix.identity();
+	
 	Matrix viewMatrix;
-	//viewMatrix.identity();
-	//modelMatrix.Translate(1.0, 1.0, 0.0);
+	
 	projectionMatrix.setOrthoProjection(-3.55, 3.55, -2.0f, 2.0f, -1.0f, 1.0f);
 	glUseProgram(program->programID);
 
+	std::string text = "";
 
-	GLuint texture1 = LoadTexture(RESOURCE_FOLDER"cardBack_blue2.png");
-	GLuint texture2 = LoadTexture(RESOURCE_FOLDER"cardBack_blue2.png");
-	GLuint texture3 = LoadTexture(RESOURCE_FOLDER"cardBack_blue2.png");
-	GLuint texture4 = LoadTexture(RESOURCE_FOLDER"cardBack_blue2.png");
+	GLuint spritesheet = LoadTexture("sheet.png");
 
-	
-	
-	GLuint spriteSheetTexture = LoadTexture("sheet.png");
-	SheetSprite  mySprite(spriteSheetTexture, 112.0f / 1024.0f, 791.0f / 1024.0f, 112.0f / 1024.0f, 75.0f / 1024.0f, 0.22, program);
+	GLuint fontTexture = LoadTexture("font1.png");
 
-	GLuint enemyBlack = LoadTexture("sheet.png");
-	SheetSprite  spr_enemy1(enemyBlack, 423.0f / 1024.0f, 728.0f / 1024.0f, 93.0f / 1024.0f, 84.0f / 1024.0f, 0.06, program);
+	SheetSprite  mySprite(spritesheet, 112.0f / 1024.0f, 791.0f / 1024.0f, 112.0f / 1024.0f, 75.0f / 1024.0f, 0.22, program);
 
-	GLuint enemyBlue = LoadTexture("sheet.png");
-	SheetSprite  spr_enemy2(enemyBlue, 425.0f / 1024.0f, 468.0f / 1024.0f, 93.0f / 1024.0f, 84.0f / 1024.0f, 0.06, program);
+	SheetSprite  spr_enemy1(spritesheet, 423.0f / 1024.0f, 728.0f / 1024.0f, 93.0f / 1024.0f, 84.0f / 1024.0f, 0.06, program);
 
-	GLuint enemyGreen = LoadTexture("sheet.png");
-	SheetSprite  spr_enemy3(enemyGreen, 425.0f / 1024.0f, 552.0f / 1024.0f, 93.0f / 1024.0f, 84.0f / 1024.0f, 0.06, program);
+	SheetSprite  spr_enemy2(spritesheet, 425.0f / 1024.0f, 468.0f / 1024.0f, 93.0f / 1024.0f, 84.0f / 1024.0f, 0.06, program);
 
-	GLuint enemyRed1 = LoadTexture("sheet.png");
-	SheetSprite  spr_enemy4(enemyRed1, 425.0f / 1024.0f, 384.0f / 1024.0f, 93.0f / 1024.0f, 84.0f / 1024.0f, 0.06, program);
+	SheetSprite  spr_enemy3(spritesheet, 425.0f / 1024.0f, 552.0f / 1024.0f, 93.0f / 1024.0f, 84.0f / 1024.0f, 0.06, program);
 
-	GLuint enemyRed2 = LoadTexture("sheet.png");
-	SheetSprite  spr_enemy5(enemyRed2, 120.0f / 1024.0f, 520.0f / 1024.0f, 104.0f / 1024.0f, 84.0f / 1024.0f, 0.06, program);
+	SheetSprite  spr_enemy4(spritesheet, 425.0f / 1024.0f, 384.0f / 1024.0f, 93.0f / 1024.0f, 84.0f / 1024.0f, 0.06, program);
 
-	
+	SheetSprite  spr_enemy5(spritesheet, 120.0f / 1024.0f, 520.0f / 1024.0f, 104.0f / 1024.0f, 84.0f / 1024.0f, 0.06, program);
 
-	Entity* anEntity = new Entity(0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 3.0f, 0.0f, -1.67f, modelMatrix, program, mySprite);
+	SheetSprite spr_bullet(spritesheet, 856.0f / 1024.0f, 421.0f / 1024.0f, 9.0f / 1024.0f, 54.0f / 1024.0f, 0.2, program);
 
+	Spaceship* spaceship = new Spaceship(0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 2.0f, 0.0f, -1.67f, modelMatrix, program, mySprite, false, false);
 
+	float xoffset = -3.2;
+	float yoffset = 1.85;
 
+	for (int i = 0; i < 11; i++) {
+		Enemy* enemy_black = new Enemy(1.0f, 1.0f, 0.0, 2.0f, 2.0f, 0.0f, xoffset, yoffset, modelMatrix, program, spr_enemy1);
 
+		//enemyMatrix.enemyVect[0][i] = enemy_black;
+		entities.push_back(enemy_black);
+		xoffset += 0.3;
+	}
+	xoffset = -3.2;
+	yoffset -= 0.2;
+	//for (int i = 0; i < 11; i++) {
+	//	Enemy* enemy_blue = new Enemy(1.0f, 1.0f, 0.0, 2.0f, 2.0f, 0.0f, xoffset, yoffset, modelMatrix, program, spr_enemy2);
+	//	//enemy_blue->posX = xoffset;
+	//	//enemy_blue->posY = yoffset;
+	//	//enemy_blue->Render();
 
+	//	//enemyMatrix.enemyVect[1][i] = enemy_blue;
+	//	entities.push_back(enemy_blue);
+
+	//	xoffset += 0.3;
+	//}
+	//xoffset = -3.2;
+	//yoffset -= 0.2;
+	//for (int i = 0; i < 11; i++) {
+	//	Enemy* enemy_green = new Enemy(1.0f, 1.0f, 0.0, 2.0f, 2.0f, 0.0f, xoffset, yoffset, modelMatrix, program, spr_enemy3);
+
+	//	//enemyMatrix.enemyVect[2][i] = enemy_green;
+	//	entities.push_back(enemy_green);
+	//	xoffset += 0.3;
+	//}
+
+	//xoffset = -3.2;
+	//yoffset -= 0.2;
+	//for (int i = 0; i < 11; i++) {
+	//	Enemy* enemy_red1 = new Enemy(1.0f, 1.0f, 0.0, 2.0f, 2.0f, 0.0f, xoffset, yoffset, modelMatrix, program, spr_enemy4);
+
+	//	//enemyMatrix.enemyVect[3][i] = enemy_red1;
+	//	entities.push_back(enemy_red1);
+	//	xoffset += 0.3;
+	//}
+
+	//xoffset = -3.2;
+	//yoffset -= 0.2;
+
+	//for (int i = 0; i < 11; i++) {
+	//	Enemy* enemy_red2 = new Enemy(1.0f, 1.0f, 0.0, 2.0f, 2.0f, 0.0f, xoffset, yoffset, modelMatrix, program, spr_enemy5);
+
+	//	entities.push_back(enemy_red2);
+	//	//enemyMatrix.enemyVect[4][i] = enemy_red2;
+	//	xoffset += 0.3;
+	//}
+	float angle = 0;
+
+	program->setProjectionMatrix(projectionMatrix);
+	program->setViewMatrix(viewMatrix);
 
 	SDL_Event event;
 	bool done = false;
+
 	while (!done) {
+		const Uint8 *keys = SDL_GetKeyboardState(NULL);
 		while (SDL_PollEvent(&event)) {
 			if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE) {
 				done = true;
+			}
+			if (keys[SDL_SCANCODE_SPACE]) {
+				shootBullet(program, spr_bullet, spaceship, modelMatrix);
 			}
 		}
 		float ticks = (float)SDL_GetTicks() / 1000.0f;
 		float elapsed = ticks - lastFrameTicks;
 		lastFrameTicks = ticks;
 
-		//translations[4] += elapsed;
 
 		glClear(GL_COLOR_BUFFER_BIT);
-		//program->setModelMatrix(modelMatrix);
-		//program->setProjectionMatrix(projectionMatrix);
-		//program->setViewMatrix(viewMatrix);
-
-		//modelMatrix.identity();
 
 
-		//glBindTexture(GL_TEXTURE_2D, texture1);
-		//float vertices[] = { -0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5, 0.5 };
-		//glVertexAttribPointer(program->positionAttribute, 2, GL_FLOAT, false, 0, vertices);
-		//glEnableVertexAttribArray(program->positionAttribute);
-		//float texCoords[] = { 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0 };
-		//glVertexAttribPointer(program->texCoordAttribute, 2, GL_FLOAT, false, 0, texCoords);
-		//glEnableVertexAttribArray(program->texCoordAttribute);
-		//glDrawArrays(GL_TRIANGLES, 0, 6);
-		//glDisableVertexAttribArray(program->positionAttribute);
-		//glDisableVertexAttribArray(program->texCoordAttribute);
-		//modelMatrix.Translate(1.0, 1.0, 0.0);
+		float minX = -3.5f;
+		float maxX = 3.5f;
+		if (keys[SDL_SCANCODE_LEFT] && (spaceship->x - (spaceship->width / 2.0) >= minX)) {
+			spaceship->movingLeft = true;
+			spaceship->movingRight = false;
+			spaceship->x += (-1 * elapsed * spaceship->speed);
+		}
+		if (keys[SDL_SCANCODE_RIGHT] && (spaceship->x + (spaceship->width / 2.0) <= maxX)) {
+			spaceship->movingLeft = false;
+			spaceship->movingRight = true;
+			spaceship->x += (1 * elapsed * spaceship->speed);
+		}
+		
+		
+		Update(program, elapsed,  spaceship, entities, modelMatrix, text);
+		Render(program, modelMatrix, text, spaceship, entities, fontTexture, 2.3f, 0.05f);
 
 
-		program->setModelMatrix(modelMatrix);
-		program->setProjectionMatrix(projectionMatrix);
-		program->setViewMatrix(viewMatrix);
-
-		modelMatrix.identity();
-		modelMatrix.Translate(1.0, -1.0, 0.0);
-		//modelMatrix.Scale(anEntity->width, anEntity->height, 0.0);
-		//modelMatrix.Scale(2.0, 2.0, 0.0);
-		anEntity->drawSprite();
-
-		/*if (translations[1] != 1.9)
-		translations[1] += (0.2 *elapsed);
-		else { translations[1] = -1.0; }
-		if (translations[2] != 2.0) translations[2] += (0.2 *elapsed);
-		else { translations[2] = -1.0; }
-		if (translations[3] != 1.8) translations[3] += (0.2 *elapsed);
-		else { translations[1] = -1.0; }
-		if (translations[4] != 2.0) translations[4] += (0.2 *elapsed);
-		else { translations[4] = -1.0; }
-		if (translations[5] != 1.7) translations[5] -= (0.3 *elapsed);
-		else { translations[1] = 1.0; }
-		*/
 
 
-		/*
-		translations = { 1.0, -1.0,
-		-1.0, -1.0,
-		-1.0, 1.0, };*/
 
 		SDL_GL_SwapWindow(displayWindow);
 
